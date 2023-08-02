51c44e798c620e0c5911b3a1ab52ff7f257da5ca
gellisun <uengyaf@hotmail.it>
2023-08-02 11:14:17 +0400

    models and detail page - part 2 of the lab

--- marvelcollector/main_app/models.py
+++ marvelcollector/main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+class Comic(models.Model):
+    name = models.CharField(max_length=100)
+    published = models.DateField()
+    writer = models.CharField(max_length=100)
+    penciler = models.CharField(max_length=100)
+    cover = models.CharField(max_length=100)
+    img_url = models.URLField()
+    description = models.TextField(max_length=1000)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
--- marvelcollector/main_app/views.py
+++ marvelcollector/main_app/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 
-comics = [
-    {'name': 'The Amazing Spider-Man (2018) #1', 'published': '2018-07-11', 'writer': 'Nick Spencer', 'penciler': 'Ryan Ottley, Humberto Ramos', 'cover': 'Ryan Ottley', 'img_url': 'https://i.imgur.com/GHZZCPh.png'},
-    {'name': 'Avengers (2018) #1', 'published': '2018-05-02', 'writer': 'Jason Aaron', 'penciler': 'Ed Mcguinness', 'cover': 'Ed Mcguinness', 'img_url': 'https://i.imgur.com/q2eR88c.png'},
-    {'name': 'Black Widow (2014)) #1', 'published': '2014-01-08', 'writer': 'Nathan Edmonson', 'penciler': '', 'cover': 'Philip J. Noto', 'img_url': 'https://i.imgur.com/4O2dsEo.png'},
-    {'name': 'Vision (2015) #1', 'published': '2015-11-04', 'writer': 'Tom King', 'penciler': '', 'cover': 'Mike Del Mundo', 'img_url': 'https://i.imgur.com/FjoFUKn.png'},
-]
+from .models import Comic
 
 def home(request):
     return render(request, 'home.html')
@@ -14,4 +9,9 @@ def about(request):
     return render(request, 'about.html')
 
 def comics_index(request):
+    comics = Comic.objects.all()
     return render(request, 'comics/index.html', {'comics': comics})
+
+def comics_detail(request, comic_id):
+    comic = Comic.objects.get(id=comic_id)
+    return render(request, 'comics/detail.html', {'comic': comic})
